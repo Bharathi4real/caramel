7ef5a03d8c8776d25f81c8064059321d44ae9458
bharathi4real <team.ak.freefire@gmail.com>
2023-04-18 12:55:43 +0530

    added vercel deployment support

--- main.go
+++ main.go
@@ -2,21 +2,15 @@ package main
 
 import (
 	"net/http"
+
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Handler() http.Handler {
-	r := gin.Default()
-	r.Static("/", "")
-	return r
-}
-
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	http.ListenAndServe(":"+port, Handler())
+	r := gin.Default()
+	r.Static("/", "./")
+
+	http.ListenAndServe(":"+os.Getenv("PORT"), r)
 }
