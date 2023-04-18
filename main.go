93db9c79f18288f481d89a1e3ffda65b8d49823f
bharathi4real <team.ak.freefire@gmail.com>
2023-04-18 17:24:36 +0530

    updated go server

--- main.go
+++ main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin" // gin framework
 )
 
+// Handler function to return gin engine
 func Handler() http.Handler {
 	r := gin.Default()
 	r.Static("/", "")
 	return r
 }
 
+// server start function
+func startServer(addr string, handler http.Handler) {
+	log.Printf("\n\n Serever Running on %s \n\n", addr) // log to print the server address
+	err := http.ListenAndServe(addr, Handler())
+	if err != nil {
+		log.Fatalf("Error Starting Sever on %v, Error: %v", addr, err) // log fatal to exit the program
+	}
+}
+
+// main func to start server
 func main() {
-	port := os.Getenv("PORT")
+	port := os.Getenv("PORT") // fetch port from env variable
 	if port == "" {
 		port = "8080"
 	}
@@ -23,8 +34,8 @@ func main() {
 	addr := ":" + port
 	if port == "8080" {
 		addr = "localhost" + addr
-		fmt.Println("Listening on", addr)
 	}
 
-	http.ListenAndServe(addr, Handler())
+	handler := Handler()
+	startServer(addr, handler)
 }
